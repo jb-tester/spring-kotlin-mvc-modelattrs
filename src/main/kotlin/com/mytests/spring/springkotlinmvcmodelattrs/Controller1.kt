c0af847739979ff8209a522d08fff50c4decc703
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-07-16 10:49:44 +0200

    Controller1 and the corrsponding views

--- src/main/kotlin/com/mytests/spring/springkotlinmvcmodelattrs/Controller1.kt
+++ src/main/kotlin/com/mytests/spring/springkotlinmvcmodelattrs/Controller1.kt
@@ -15,69 +15,83 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes
 @RequestMapping("/")
 class Controller1 {
 
-    @ModelAttribute("attributeName")
+    @ModelAttribute("classLevelAttr")
     fun addAttribute(): String {
-        return "value"
+        // ok:
+        return "classLevelAttrValue"
     }
 
     @GetMapping("/example0")
     fun example0(): String {
-        return "viewName"
+        return "example0"
     }
     @GetMapping("/example1")
     fun example1(modelMap: ModelMap): String {
-        modelMap["attributeName"] = "value"
-        modelMap.addAttribute("attributeName", "value")
-        modelMap.addAllAttributes(mapOf("attr1" to "value1", "attr2" to "value2"))
-        modelMap.addAllAttributes(listOf(Pair("attr3", "value3"), Pair("attr4", "value4")))
-        return "viewName"
+        // ok:
+        modelMap["ex1_attr00"] = "ex1_value00"
+        // ok:
+        modelMap.addAttribute("ex1_attr0", "ex1_value0")
+        // these attributes are not resolved:
+        modelMap.addAllAttributes(mapOf("ex1_attr1" to "ex1_value1", "ex1_attr2" to "ex1_value2"))
+        println("ModelMap attributes: ${modelMap.keys.joinToString(", ")}")
+        println("ModelMap attributes: ${modelMap.values.joinToString(", ")}")
+        return "example1"
     }
-    @GetMapping("/example11")
-    fun test0(model: Model):String{
-        model.asMap()["attr111"] = "attr111"
-        model.addAttribute("attr112", "attr112")
-        model["attr113"] = "attr113"
-        model.addAllAttributes(mapOf("attr1" to "value1", "attr2" to "value2"))
-        model.addAllAttributes(listOf(Pair("attr3", "value3"), Pair("attr4", "value4")))
-        return "test0"
-    }
-
 
     @GetMapping("/example2")
     fun example2(): ModelAndView {
-        val modelAndView = ModelAndView("viewName")
-        modelAndView.addObject("attributeName", "value")
+        val modelAndView = ModelAndView("example2")
+        // ok:
+        modelAndView.addObject("ex2_attr1", "ex2_attr1Value")
         return modelAndView
     }
 
     @GetMapping("/example3")
-    fun example3(modelMap: MutableMap<String, Any>): String {
-        modelMap["attributeName"] = "value"
-        return "viewName"
+    fun example3(mutableMap: MutableMap<String, Any>): String {
+        // this attribute is not resolved:
+        mutableMap["ex3_attr1"] = "ex3_attr1Value"
+        return "example3"
     }
-
-
+    @GetMapping("/example4")
+    fun example4(model: Model):String{
+        // this attribute is not resolved:
+        model.asMap()["ex4_attr1"] = "ex4_attr1Value"
+        // ok:
+        model.addAttribute("ex4_attr2", "ex4_attr2Value")
+        // ok:
+        model["ex4_attr3"] = "ex4_attr3Value"
+        // these attributes are not resolved:
+        model.addAllAttributes(mapOf("ex4_attr4" to "ex4_attr4Value", "ex4_attr5" to "ex4_attr5Value"))
+        println("ModelMap attributes: ${model.asMap().keys.joinToString(", ")}")
+        println("ModelMap attributes: ${model.asMap().values.joinToString(", ")}")
+        return "example4"
+    }
+    // the correct HTTP request is not generated:
+   // GET http://localhost:8080/example5?ex5_attr1=foo
     @GetMapping("/example5")
-    fun example5(@ModelAttribute("attributeName") attribute: String): String {
+    fun example5(@ModelAttribute("ex5_attr1") attribute: String): String {
         println("Attribute value: $attribute")
-        return "viewName"
+        return "example5"
     }
 
     data class MyModel(val id: Int, val name: String)
 
+    // the view is not resolved, the model attribute there is also n/r obviously:
+    @ModelAttribute("myModel")
     @GetMapping("/example6")
     fun example6(): MyModel {
-        return MyModel(1, "Kotlin Example")
+        return MyModel(1, "example6")
     }
 
     @GetMapping("/example7")
     fun example7(redirectAttributes: RedirectAttributes): String {
-        redirectAttributes.addFlashAttribute("flashAttribute", "flashValue")
+        // this attribute is not resolved:
+        redirectAttributes["ex7_attr1"] = "ex7_attr1Value"
+        redirectAttributes.addFlashAttribute("ex7_flashAttribute", "ex7_flashValue")
         return "redirect:/anotherEndpoint"
     }
-
     @GetMapping("/anotherEndpoint")
     fun another(): String {
-        return "viewName"
+        return "example7"
     }
 }
